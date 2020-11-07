b1776f5f25173e54c44b112b5edc46e28d3ea4bf
Eugene Theriault <hybridbassist@gmail.com>
2020-11-06 19:07:55 -0800

    Add commented out schema cheatsheet to bulk_discount

--- app/models/bulk_discount.rb
+++ app/models/bulk_discount.rb
@@ -1,3 +1,12 @@
 class BulkDiscount < ApplicationRecord
   belongs_to :user
 end
+
+
+# BulkDiscount as of version 2020_11_07_025154
+#  create_table "bulk_discounts", force: :cascade do |t|
+#    t.float "percent"
+#    t.integer "required_quantity"
+#    t.bigint "user_id"
+#    t.index ["user_id"], name: "index_bulk_discounts_on_user_id"
+#  end

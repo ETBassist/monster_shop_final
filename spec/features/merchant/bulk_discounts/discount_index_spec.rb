d9fd53b49092d8034da1ebc07946ff67fc32e90c
Eugene Theriault <hybridbassist@gmail.com>
2020-11-06 19:58:58 -0800

    Create test to make sure that other merchants discounts are not shown

--- spec/features/merchant/bulk_discounts/discount_index_spec.rb
+++ spec/features/merchant/bulk_discounts/discount_index_spec.rb
@@ -5,21 +5,31 @@ feature 'Bulk Discount Index' do
   describe "As a merchant admin when I visit my bulk items index" do
     before :each do
       page.set_rack_session(user_id: @merchant_admin.id)
+      @merchant_admin2 = create(:user, role: 1)
       @discount1 = @merchant_admin.bulk_discounts.create!(percent: 0.05,
-                                             required_quantity: 10)
+                                                          required_quantity: 10)
       @discount2 = @merchant_admin.bulk_discounts.create!(percent: 0.55,
-                                             required_quantity: 20)
+                                                          required_quantity: 20)
+      @discount3 = @merchant_admin2.bulk_discounts.create!(percent: 0.55,
+                                                           required_quantity: 20)
     end
 
     it "I see all the bulk discounts I've created" do
       visit '/merchant/bulk_discounts'
 
-      expect(page).to have_content(@discount1.id)
-      expect(page).to have_content(@discount1.percent)
-      expect(page).to have_content(@discount1.required_quantity)
-      expect(page).to have_content(@discount2.id)
-      expect(page).to have_content(@discount2.percent)
-      expect(page).to have_content(@discount2.required_quantity)
+      within("#discount-#{@discount1.id}") do
+        expect(page).to have_content(@discount1.id)
+        expect(page).to have_content(@discount1.percent)
+        expect(page).to have_content(@discount1.required_quantity)
+      end
+
+      within("#discount-#{@discount2.id}") do
+        expect(page).to have_content(@discount2.id)
+        expect(page).to have_content(@discount2.percent)
+        expect(page).to have_content(@discount2.required_quantity)
+      end
+
+      expect(page).to_not have_selector("#discount-#{@discount3.id}")
     end
   end
 end

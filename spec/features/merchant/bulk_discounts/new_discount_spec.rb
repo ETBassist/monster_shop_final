b7be30756dad8be189aff99480c553fd96890cc7
Eugene Theriault <hybridbassist@gmail.com>
2020-11-06 21:10:42 -0800

    Create test for sad path missing field

--- spec/features/merchant/bulk_discounts/new_discount_spec.rb
+++ spec/features/merchant/bulk_discounts/new_discount_spec.rb
@@ -16,5 +16,14 @@ feature 'New Discount Page' do
       expect(current_path).to eq('/merchant/bulk_discounts')
       expect(page).to have_content("Discount Created")
     end
+
+    it "if I fill in a form incorrectly I'm returned to the page and I see a message" do
+      visit '/merchant/bulk_discounts/new'
+
+      fill_in(:percent, with: 10)
+      click_button("Create Bulk Discount")
+      expect(current_path).to eq('/merchant/bulk_discounts/new')
+      expect(page).to have_content("Missing Required Field")
+    end
   end
 end

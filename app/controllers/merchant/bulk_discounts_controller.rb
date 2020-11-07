32c830a1527c83cfb9d50a76c44d46e48d331e85
Eugene Theriault <hybridbassist@gmail.com>
2020-11-06 19:21:07 -0800

    Create discount index view

--- app/controllers/merchant/bulk_discounts_controller.rb
+++ app/controllers/merchant/bulk_discounts_controller.rb
@@ -1,4 +1,5 @@
 class Merchant::BulkDiscountsController < ApplicationController
   def index
+    @discounts = BulkDiscount.where(user_id: current_user.id)
   end
 end
--- spec/features/merchant/bulk_discounts/discount_index_spec.rb
+++ spec/features/merchant/bulk_discounts/discount_index_spec.rb
@@ -5,11 +5,21 @@ feature 'Bulk Discount Index' do
   describe "As a merchant admin when I visit my bulk items index" do
     before :each do
       page.set_rack_session(user_id: @merchant_admin.id)
+      @discount1 = @merchant_admin.bulk_discounts.create!(percent: 0.05,
+                                             required_quantity: 10)
+      @discount2 = @merchant_admin.bulk_discounts.create!(percent: 0.55,
+                                             required_quantity: 20)
     end
 
     it "I see all the bulk discounts I've created" do
       visit '/merchant/bulk_discounts'
 
+      expect(page).to have_content(@discount1.id)
+      expect(page).to have_content(@discount1.percent)
+      expect(page).to have_content(@discount1.required_quantity)
+      expect(page).to have_content(@discount2.id)
+      expect(page).to have_content(@discount2.percent)
+      expect(page).to have_content(@discount2.required_quantity)
     end
   end
 end

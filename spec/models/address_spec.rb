361c5d18d218cb5b2bbeb800aede75efe1fec400
Eugene Theriault <hybridbassist@gmail.com>
2020-11-09 12:54:48 -0800

    Add user -> address relationship and create tests for address validations

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -4,4 +4,12 @@ RSpec.describe Address, type: :model do
   describe 'relationships' do
     it { should belong_to :user }
   end
+
+  describe 'validations' do
+    it { should validate_presence_of :nickname }
+    it { should validate_presence_of :address }
+    it { should validate_presence_of :city }
+    it { should validate_presence_of :state }
+    it { should validate_presence_of :zip }
+  end
 end

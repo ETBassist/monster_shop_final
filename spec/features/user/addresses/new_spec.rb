d1f6b25610815d4e7c1c994fa024388869421acc
Eugene Theriault <hybridbassist@gmail.com>
2020-11-09 13:46:02 -0800

    Create sad path test for address creation

--- spec/features/user/addresses/new_spec.rb
+++ spec/features/user/addresses/new_spec.rb
@@ -36,5 +36,22 @@ feature "User/Addresses/New" do
       expect(@user.addresses.last.state).to eq("YO")
       expect(@user.addresses.last.zip).to eq(12345)
     end
+
+    it "if I don't fill all the fields in I see a message and am returned to the page" do
+      visit '/profile/addresses/new'
+
+      fill_in(:nickname, with: "My Crib")
+      fill_in(:address, with: "123 Dope Street")
+      fill_in(:city, with: "Awesome Town")
+      fill_in(:state, with: "YO")
+
+      click_button("Create Address")
+
+      expect(page).to have_content("Zip can't be blank")
+      expect(page).to have_field(:nickname, with: "My Crib")
+      expect(page).to have_field(:address, with: "123 Dope Street")
+      expect(page).to have_field(:city, with: "Awesome Town")
+      expect(page).to have_field(:state, with: "YO")
+    end
   end
 end

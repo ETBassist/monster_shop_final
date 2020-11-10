853f3e662238fb743aa357b3a98fc4bcaf5cd59f
Eugene Theriault <hybridbassist@gmail.com>
2020-11-10 09:58:08 -0800

    Create migration to break all the tests by removing user address columns

---
+++ db/migrate/20201110175510_remove_address_from_user.rb
@@ -0,0 +1,8 @@
+class RemoveAddressFromUser < ActiveRecord::Migration[5.2]
+  def change
+    remove_column :users, :address
+    remove_column :users, :city
+    remove_column :users, :state
+    remove_column :users, :zip
+  end
+end

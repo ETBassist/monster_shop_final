2cc085cf08feb3a26cd2caf9e28ad667cf7a7333
Eugene Theriault <hybridbassist@gmail.com>
2020-11-09 18:17:12 -0800

    Create test for method to check if an address can be deleted

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -13,4 +13,33 @@ RSpec.describe Address, type: :model do
     it { should validate_presence_of :state }
     it { should validate_presence_of :zip }
   end
+
+  describe 'instance methods' do
+    before :each do
+      @user = create(:user)
+      @address1 = @user.addresses.create(nickname: "My Crib",
+                                        address: "123 Dope Street",
+                                        city: "Boulder",
+                                        state: "CO",
+                                        zip: 12345)
+      @address2 = @user.addresses.create(nickname: "Home",
+                                        address: "246 Cozy Lane",
+                                        city: "Boulder",
+                                        state: "CO",
+                                        zip: 12346)
+      @order1 = @address1.orders.create!(user_id: @user.id,
+                                       status: 1)
+      @order2 = @address2.orders.create!(user_id: @user.id,
+                                       status: 2)
+      @order3 = @address1.orders.create!(user_id: @user.id,
+                                       status: 3)
+      @order4 = @address1.orders.create!(user_id: @user.id,
+                                       status: 0)
+    end
+
+    it "#can_be_deleted?" do
+      expect(@address1.can_be_deleted?).to eq(false)
+      expect(@address2.can_be_deleted?).to eq(true)
+    end
+  end
 end

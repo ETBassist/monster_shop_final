2b1aba7c2da22cd9ee597a4392319f6de4d09589
Eugene Theriault <hybridbassist@gmail.com>
2020-11-09 16:38:03 -0800

    Create has_many relations between order and address

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,5 +1,7 @@
 class Address < ApplicationRecord
   belongs_to :user
+  has_many :order_addresses
+  has_many :orders, through: :order_addresses
 
   validates_presence_of :nickname, :address, :city, :state, :zip
 end

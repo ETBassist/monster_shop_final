4eafc58d0fa7ce1791638c21863234d887805fa5
Eugene Theriault <hybridbassist@gmail.com>
2020-11-09 16:37:11 -0800

    Add test for address -> order relations

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Address, type: :model do
   describe 'relationships' do
     it { should belong_to :user }
+    it { should have_many(:orders).through(:order_addresses) }
   end
 
   describe 'validations' do
